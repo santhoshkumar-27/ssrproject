c498b7c5712c5dd09963a7f2a063f3d735f3ccf0
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-11-18 12:57:29 +0530

    angular project initial setup for ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ssrproject';
+  clickHandler() {
+    console.log('this is click')
+    localStorage.setItem('key for ssr', '3214564464 asdfa4sf564ad56f465da4f')
+  }
 }
